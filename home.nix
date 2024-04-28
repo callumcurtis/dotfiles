17cafc307aba99546e6d07bf762a7369cee75fb3
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-04-27 23:15:56 -0700

    initial commit

---
+++ home.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "callumcurtis";
+  home.homeDirectory = "/home/callumcurtis";
+
+  # Packages installed to the user profile.
+  home.packages = with pkgs; [
+    vim
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Callum Curtis";
+    userEmail = "105407637+callumcurtis@users.noreply.github.com";
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
