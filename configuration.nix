31dd4bddd04b23aa0e9fd99462860e06be168cd9
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-04-28 17:50:15 -0700

    remove xterm and gnome-console

--- configuration.nix
+++ configuration.nix
@@ -32,10 +32,12 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
+  services.xserver.excludePackages = with pkgs; [ xterm ];
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  environment.gnome.excludePackages = with pkgs; [ gnome-console ];
 
   # Configure keymap in X11
   services.xserver = {
