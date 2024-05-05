a7deebfd0b03a6b5ad52791b4e1cd8c484d861a6
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-05-04 20:24:53 -0400

    set udev rules for configuring ulx from xpanel

--- configuration.nix
+++ configuration.nix
@@ -6,8 +6,9 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
+      ./ulx-configuration.nix
     ];
 
   # Bootloader.
---
+++ ulx-configuration.nix
@@ -0,0 +1,16 @@
+{
+  # Allows XPANEL to modify UltralightX settings from the browser (if given permission)
+  services.udev.extraRules = ''
+    # Rules from https://github.com/teamfinalmouse/xpanel-linux-permissions/blob/02a498fc9c9f39e73fa8477e3af1f2032812e98f/99-finalmouse.rules
+
+    SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="361d", ATTR{idProduct}=="0100", MODE="0666"
+    SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="361d", ATTR{idProduct}=="0101", MODE="0666"
+    SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="361d", ATTR{idProduct}=="0102", MODE="0666"
+    SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="361d", ATTR{idProduct}=="0103", MODE="0666"
+    SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="361d", ATTR{idProduct}=="0111", MODE="0666"
+
+    KERNEL=="hidraw*", ATTRS{idVendor}=="361d", ATTRS{idProduct}=="0100", MODE="0666"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="361d", ATTRS{idProduct}=="0101", MODE="0666"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="361d", ATTRS{idProduct}=="0102", MODE="0666"
+  '';
+}
