03b6aaf52c6f3a8ce40617fb6473371980f7e13b
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-05-16 19:57:03 -0400

    inline user config in default workstation module

--- roles/workstation/default.nix
+++ roles/workstation/default.nix
@@ -1,4 +1,95 @@
 {
-  user = ./user.nix;
   system = ./system.nix;
+
+  user = { pkgs, ... }: {
+    fonts.fontconfig.enable = true;
+
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        document-font-name = "Liberation Sans 10";
+        font-name = "Liberation Sans 10";
+        monospace-font-name = "Liberation Mono 9";
+     };
+    };
+
+    home.packages = with pkgs; [
+      lunarvim
+      google-chrome
+      (pkgs.nerdfonts.override { fonts = ["FiraMono"]; })
+    ];
+
+    programs.fish = {
+      enable = true;
+      functions = {
+        fish_greeting = "";
+      };
+    };
+
+    programs.zoxide.enable = true;
+
+    programs.starship.enable = true;
+
+    programs.zellij = {
+      enable = true;
+      enableFishIntegration = true;
+      settings = {
+        default_shell = "${pkgs.fish}/bin/fish";
+        theme = "tokyo-night-dark";
+        default_layout = "compact";
+        pane_frames = false;
+      };
+    };
+
+    programs.eza = {
+      enable = true;
+      icons = true;
+      git = true;
+    };
+
+    programs.git = {
+      enable = true;
+      userName = "Callum Curtis";
+      userEmail = "105407637+callumcurtis@users.noreply.github.com";
+      extraConfig = {
+        rerere.enabled = true;
+        init.defaultBranch = "main";
+      };
+      delta = {
+        enable = true;
+        options = {
+          side-by-side = true;
+        };
+      };
+    };
+
+    programs.kitty = {
+      enable = true;
+      font = {
+        name = "FiraMono Nerd Font";
+        size = 10;
+      };
+      theme = "Tokyo Night Moon";
+      settings = {
+        shell = "${pkgs.fish}/bin/fish";
+      };
+    };
+
+    home.sessionVariables = {
+      EDITOR = "${pkgs.lunarvim}/bin/lvim";
+    };
+
+    # This value determines the home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update home Manager without changing this value. See
+    # the home Manager release notes for a list of state version
+    # changes in each release.
+    home.stateVersion = "23.11";
+
+    # Let home Manager install and manage itself.
+    programs.home-manager.enable = true;
+  };
 }
+
