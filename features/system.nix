3c0382806477db1085e9bcfdfaff70d3f6132022
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-05-20 21:23:55 -0400

    import system features dynamically

--- features/system.nix
+++ features/system.nix
@@ -1,15 +1,6 @@
+{ dotfiles, ... }:
+
 {
-  imports = [
-    ./fish/system.nix
-    ./networking/system.nix
-    ./printing/system.nix
-    ./audio/system.nix
-    ./flakes/system.nix
-    ./ssh/system.nix
-    ./time/system.nix
-    ./multi-boot/system.nix
-    ./internationalization/system.nix
-    ./unpatched-binaries/system.nix
-  ];
+  imports = dotfiles.lib.findMatchingFilesInDirectChildDirectories ./. "system.nix";
 }
 
