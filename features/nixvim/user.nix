9ae5e80d40cc001d0ec0923591fc15ed9d282ab8
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-08-08 21:33:33 -0400

    feat: add nixvim options

--- features/nixvim/user.nix
+++ features/nixvim/user.nix
@@ -8,6 +8,45 @@
   options.dotfiles.features.nixvim.enable = lib.mkEnableOption "nixvim";
 
   config = lib.mkIf config.dotfiles.features.nixvim.enable {
-    programs.nixvim.enable = true;
+    programs.nixvim = {
+      enable = true;
+
+      opts = {
+        # line numbering
+        relativenumber = true;
+        number = true;
+
+        # whitespace
+        tabstop = 2;
+        shiftwidth = 2;
+        expandtab = true;
+        autoindent = true;
+        signcolumn = "yes";
+
+        # line wrapping
+        wrap = false;
+
+        # search
+        ignorecase = true;
+        smartcase = true;
+
+        # line highlighting
+        cursorline = true;
+
+        # theming
+        termguicolors = true;
+        background = "dark";
+
+        # backspace
+        backspace = "indent,eol,start";
+
+        # clipboard
+        clipboard = "unnamedplus";
+
+        # windows
+        splitright = true;
+        splitbelow = true;
+      };
+    };
   };
 }
