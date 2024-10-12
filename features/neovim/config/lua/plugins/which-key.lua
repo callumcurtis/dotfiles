e94b372c7e45aa20984d51856e5d5c1f517c61cb
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-10-11 22:17:45 -0400

    docs: add todo to use groups from keymaps

--- features/neovim/config/lua/plugins/which-key.lua
+++ features/neovim/config/lua/plugins/which-key.lua
@@ -1,4 +1,5 @@
 -- TODO: update this placeholder configuration
+-- TODO: use groups from keymaps
 return {
   -- which-key helps you remember key bindings by showing a popup
   -- with the active keybindings of the command you started typing.
