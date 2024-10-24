c4a22cc8cac685119580bae6c85df496eeacdb18
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-10-23 22:41:01 -0400

    fix: swap to auto-cpufreq with thermald

--- hosts/mist/hardware.nix
+++ hosts/mist/hardware.nix
@@ -24,7 +24,8 @@
 
   swapDevices = [ ];
 
-  powerManagement.cpuFreqGovernor = "performance";
+  services.auto-cpufreq.enable = true;
+  services.thermald.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
