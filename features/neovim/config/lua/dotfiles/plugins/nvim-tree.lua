28c5b834098967a746076c986f180870e15f59a4
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-11-10 22:19:11 -0500

    refactor: unnest neovim plugin definitions

--- features/neovim/config/lua/dotfiles/plugins/nvim-tree.lua
+++ features/neovim/config/lua/dotfiles/plugins/nvim-tree.lua
@@ -1,138 +1,136 @@
 return {
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
+  "nvim-tree/nvim-tree.lua",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-      local api = require("nvim-tree.api")
-      local icons = require("dotfiles.icons")
-      local keymap = vim.keymap
+    local api = require("nvim-tree.api")
+    local icons = require("dotfiles.icons")
+    local keymap = vim.keymap
 
-      require("nvim-tree").setup({
-        on_attach = function(bufnr)
-          local opts = function(desc)
-            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    require("nvim-tree").setup({
+      on_attach = function(bufnr)
+        local opts = function(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        local function edit_or_open()
+          local node = api.tree.get_node_under_cursor()
+          if node.nodes ~= nil then
+            -- expand or collapse folder
+            api.node.open.edit()
+          else
+            -- open file
+            api.node.open.edit()
+            -- Close the tree if file was opened
+            api.tree.close()
           end
+        end
 
-          local function edit_or_open()
-            local node = api.tree.get_node_under_cursor()
-            if node.nodes ~= nil then
-              -- expand or collapse folder
-              api.node.open.edit()
-            else
-              -- open file
-              api.node.open.edit()
-              -- Close the tree if file was opened
-              api.tree.close()
-            end
-          end
-
-          -- TODO: implement recursive subfolder close/open bound to H/L keys
-          -- See: https://github.com/nvim-tree/nvim-tree.lua/blob/f5f67892996b280ae78b1b0a2d07c4fa29ae0905/lua/nvim-tree/actions/tree/modifiers/collapse-all.lua
-          keymap.set("n", "l", edit_or_open, opts("Open"))
-        end,
-        view = {
-          width = 30,
-          adaptive_size = true,
-          cursorline = true,
-          side = "left",
-          number = true,
-          relativenumber = true,
-          signcolumn = "yes",
+        -- TODO: implement recursive subfolder close/open bound to H/L keys
+        -- See: https://github.com/nvim-tree/nvim-tree.lua/blob/f5f67892996b280ae78b1b0a2d07c4fa29ae0905/lua/nvim-tree/actions/tree/modifiers/collapse-all.lua
+        keymap.set("n", "l", edit_or_open, opts("Open"))
+      end,
+      view = {
+        width = 30,
+        adaptive_size = true,
+        cursorline = true,
+        side = "left",
+        number = true,
+        relativenumber = true,
+        signcolumn = "yes",
+      },
+      sort = {
+        sorter = "name",
+        folders_first = true,
+        files_first = false,
+      },
+      renderer = {
+        symlink_destination = true,
+        indent_markers = {
+          enable = false,
+          inline_arrows = true,
         },
-        sort = {
-          sorter = "name",
-          folders_first = true,
-          files_first = false,
-        },
-        renderer = {
-          symlink_destination = true,
-          indent_markers = {
-            enable = false,
-            inline_arrows = true,
-          },
-          icons = {
-            webdev_colors = true,
-            web_devicons = {
-              file = {
-                enable = true,
-                color = true,
-              },
-              folder = {
-                enable = false,
-              },
+        icons = {
+          webdev_colors = true,
+          web_devicons = {
+            file = {
+              enable = true,
+              color = true,
             },
-            git_placement = "after",
-            padding = " ",
-            symlink_arrow = " ➛ ",
-            modified_placement = "after",
-            diagnostics_placement = "signcolumn",
-            bookmarks_placement = "signcolumn",
-            show = {
-              file = true,
-              folder = true,
-              folder_arrow = true,
-              git = true,
-              modified = true,
-              diagnostics = true,
-              bookmarks = true,
-            },
-            glyphs = {
-              default = icons.ui.Text,
-              symlink = icons.ui.FileSymlink,
-              bookmark = icons.ui.BookMark,
-              modified = icons.ui.Circle,
-              folder = {
-                arrow_closed = icons.ui.TriangleShortArrowRight,
-                arrow_open = icons.ui.TriangleShortArrowDown,
-                default = icons.ui.Folder,
-                open = icons.ui.FolderOpen,
-                empty = icons.ui.EmptyFolder,
-                empty_open = icons.ui.EmptyFolderOpen,
-                symlink = icons.ui.FolderSymlink,
-                symlink_open = icons.ui.FolderOpen,
-              },
-              git = {
-                unstaged = icons.git.FileUnstaged,
-                staged = icons.git.FileStaged,
-                unmerged = icons.git.FileUnmerged,
-                renamed = icons.git.FileRenamed,
-                untracked = icons.git.FileUntracked,
-                deleted = icons.git.FileDeleted,
-                ignored = icons.git.FileIgnored,
-              },
-            },
-          },
-        },
-        diagnostics = {
-          enable = true,
-          show_on_dirs = true,
-          show_on_open_dirs = true,
-          debounce_delay = 50,
-          severity = {
-            min = vim.diagnostic.severity.HINT,
-            max = vim.diagnostic.severity.ERROR,
-          },
-          icons = {
-            hint = icons.diagnostics.BoldHint,
-            info = icons.diagnostics.BoldInformation,
-            warning = icons.diagnostics.BoldWarning,
-            error = icons.diagnostics.BoldError,
-          },
-        },
-        actions = {
-          open_file = {
-            window_picker = {
+            folder = {
               enable = false,
             },
           },
+          git_placement = "after",
+          padding = " ",
+          symlink_arrow = " ➛ ",
+          modified_placement = "after",
+          diagnostics_placement = "signcolumn",
+          bookmarks_placement = "signcolumn",
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = true,
+            modified = true,
+            diagnostics = true,
+            bookmarks = true,
+          },
+          glyphs = {
+            default = icons.ui.Text,
+            symlink = icons.ui.FileSymlink,
+            bookmark = icons.ui.BookMark,
+            modified = icons.ui.Circle,
+            folder = {
+              arrow_closed = icons.ui.TriangleShortArrowRight,
+              arrow_open = icons.ui.TriangleShortArrowDown,
+              default = icons.ui.Folder,
+              open = icons.ui.FolderOpen,
+              empty = icons.ui.EmptyFolder,
+              empty_open = icons.ui.EmptyFolderOpen,
+              symlink = icons.ui.FolderSymlink,
+              symlink_open = icons.ui.FolderOpen,
+            },
+            git = {
+              unstaged = icons.git.FileUnstaged,
+              staged = icons.git.FileStaged,
+              unmerged = icons.git.FileUnmerged,
+              renamed = icons.git.FileRenamed,
+              untracked = icons.git.FileUntracked,
+              deleted = icons.git.FileDeleted,
+              ignored = icons.git.FileIgnored,
+            },
+          },
         },
-      })
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        show_on_open_dirs = true,
+        debounce_delay = 50,
+        severity = {
+          min = vim.diagnostic.severity.HINT,
+          max = vim.diagnostic.severity.ERROR,
+        },
+        icons = {
+          hint = icons.diagnostics.BoldHint,
+          info = icons.diagnostics.BoldInformation,
+          warning = icons.diagnostics.BoldWarning,
+          error = icons.diagnostics.BoldError,
+        },
+      },
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
+    })
 
-      keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-      keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "Focus current file in explorer" })
-    end
-  }
+    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "Focus current file in explorer" })
+  end,
 }
--- features/neovim/config/lua/dotfiles/plugins/which-key.lua
+++ features/neovim/config/lua/dotfiles/plugins/which-key.lua
@@ -3,14 +3,12 @@
 return {
   -- which-key helps you remember key bindings by showing a popup
   -- with the active keybindings of the command you started typing.
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-      plugins = { spelling = true },
-      defaults = {
-        mode = { "n" },
-      },
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    plugins = { spelling = true },
+    defaults = {
+      mode = { "n" },
     },
   },
 }
