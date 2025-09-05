193938fb7dc9753b8e72d96bb817397fcf4350da
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2025-09-05 09:19:07 -0400

    feat: expand shell aliases for git operations

--- features/git/user.nix
+++ features/git/user.nix
@@ -30,6 +30,11 @@
 
     home.shellAliases = {
       gs = "git status";
+      gsl = "git stash list";
+      gss = "git stash save -m";
+      gsss = "git stash save --staged -m";
+      gsp = "git stash pop";
+      gsa = "git stash apply";
       gl = "git log --oneline";
       gll = "git log";
       gd = "git diff";
@@ -40,7 +45,10 @@
       gca = "git commit --amend";
       gco = "git checkout";
       gcob = "git checkout -b";
-      gpo = "git push origin";
+      gpo = "git push origin --force-with-lease";
+      # TODO: add "gpos" which pushes all branches in the stack
+      grb = "git rebase -i --update-refs";
+      grl = "git reflog";
     };
   };
 }
