44c527026152e05aab950ba6fd912f12e2ab9629
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2025-09-09 10:26:05 -0700

    refactor: remove unused home manager override for stylix

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url = "github:nix-community/stylix/release-25.05";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
-    stylix.inputs.home-manager.follows = "home-manager";
     wallpapers.url = "github:input-output-hk/empty-flake?rev=2040a05b67bf9a669ce17eca56beb14b4206a99a";
   };
 
