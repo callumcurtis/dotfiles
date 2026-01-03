bbbbcec8e1478794fc0a7ab00c9991504e0b172e
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2026-01-02 17:30:07 -0800

    feat: upgrade to 25.11

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "NixOS and Home Manager configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    stylix.url = "github:nix-community/stylix/release-25.05";
+    stylix.url = "github:nix-community/stylix/release-25.11";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
     wallpapers.url = "git+ssh://git@github.com/callumcurtis/wallpapers";
     hyprland.url = "github:hyprwm/Hyprland";
