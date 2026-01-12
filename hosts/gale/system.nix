4e18e4c82b34afd9fcef2d37af1e80f5cbaff61a
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2026-01-12 13:15:19 -0800

    feat: use template nix darwin

--- hosts/gale/system.nix
+++ hosts/gale/system.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ self, ... }:
 
 let
   # Assign usernames to variables to avoid typos between usages.
@@ -19,12 +19,7 @@ in
   dotfiles.system.roles.workstation.enable = true;
   dotfiles.users.${callumcurtis}.roles.workstation.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11";
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+  system.stateVersion = 6;
 }
 
