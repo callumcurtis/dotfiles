24cde24a4e1fb2e2fc944083db075f8ccf6abcbb
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2026-02-02 09:03:22 -0800

    feat: configure stylix at user level

--- features/stylix/user.nix
+++ features/stylix/user.nix
@@ -1,17 +1,43 @@
 # NOTE: the stylix module must be added to your host's list of NixOS modules.
 
-{ config, lib, ... }:
+{ pkgs, config, lib, dotfiles, ... }:
 
 {
   options.dotfiles.features.stylix = {
     enable = lib.mkEnableOption "stylix";
+    wallpaper = dotfiles.lib.mkTypedOption lib.types.path;
+    theme = dotfiles.lib.mkTypedOption lib.types.path;
   };
 
   config = lib.mkIf config.dotfiles.features.stylix.enable {
     stylix = {
+      enable = true;
       targets = {
         # Avoids: extension “user-theme@gnome-shell-extensions.gcampax.github.com” does not exist
         gnome.enable = false;
+      };
+      image = config.dotfiles.features.stylix.wallpaper;
+      polarity = "dark";
+      base16Scheme = config.dotfiles.features.stylix.theme;
+      fonts = {
+        sizes = {
+          terminal = 9;
+          desktop = 9;
+          popups = 9;
+          applications = 10;
+        };
+        monospace = {
+          package = pkgs.nerd-fonts.fira-mono;
+          name = "FiraMono";
+        };
+        sansSerif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Sans";
+        };
+        serif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Serif";
+        };
       };
     };
   };
