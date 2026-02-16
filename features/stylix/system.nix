1420dc2ebaad4625c39ff5d66cab86331f84b0fd
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2026-02-16 08:59:47 -0800

    feat: set wallpaper on darwin

--- features/stylix/system.nix
+++ features/stylix/system.nix
@@ -1,6 +1,6 @@
 # NOTE: the stylix module must be added to your host's list of NixOS modules.
 
-{ pkgs, config, lib, dotfiles, isNixos, ... }:
+{ pkgs, config, lib, dotfiles, isNixos, isDarwin, ... }:
 
 {
   options.dotfiles.features.stylix = {
@@ -9,38 +9,47 @@
     theme = dotfiles.lib.mkTypedOption lib.types.path;
   };
 
-  config = lib.mkIf config.dotfiles.features.stylix.enable {
-    stylix = {
-      enable = true;
-      image = config.dotfiles.features.stylix.wallpaper;
-      polarity = "dark";
-      base16Scheme = config.dotfiles.features.stylix.theme;
-      fonts = {
-        sizes = {
-          terminal = 9;
-          desktop = 9;
-          popups = 9;
-          applications = 10;
+  config = lib.mkIf config.dotfiles.features.stylix.enable (lib.mkMerge [
+    {
+      stylix = {
+        enable = true;
+        image = config.dotfiles.features.stylix.wallpaper;
+        polarity = "dark";
+        base16Scheme = config.dotfiles.features.stylix.theme;
+        fonts = {
+          sizes = {
+            terminal = 9;
+            desktop = 9;
+            popups = 9;
+            applications = 10;
+          };
+          monospace = {
+            package = pkgs.nerd-fonts.fira-mono;
+            name = "FiraMono";
+          };
+          sansSerif = {
+            package = pkgs.dejavu_fonts;
+            name = "DejaVu Sans";
+          };
+          serif = {
+            package = pkgs.dejavu_fonts;
+            name = "DejaVu Serif";
+          };
         };
-        monospace = {
-          package = pkgs.nerd-fonts.fira-mono;
-          name = "FiraMono";
-        };
-        sansSerif = {
-          package = pkgs.dejavu_fonts;
-          name = "DejaVu Sans";
-        };
-        serif = {
-          package = pkgs.dejavu_fonts;
-          name = "DejaVu Serif";
+      } // lib.optionalAttrs isNixos {
+        cursor = {
+          package = pkgs.bibata-cursors;
+          name = "Bibata-Original-Ice";
+          size = 24;
         };
       };
-    } // lib.optionalAttrs isNixos {
-      cursor = {
-        package = pkgs.bibata-cursors;
-        name = "Bibata-Original-Ice";
-        size = 24;
-      };
-    };
-  };
+    }
+    (lib.optionalAttrs isDarwin {
+      # Stylix cannot set the wallpaper on darwin; use desktoppr.
+      system.activationScripts.postActivation.text = ''
+        loggedInUser=$(/usr/bin/stat -f "%Su" /dev/console)
+        /usr/bin/sudo -u "$loggedInUser" ${pkgs.desktoppr}/bin/desktoppr ${config.dotfiles.features.stylix.wallpaper}
+      '';
+    })
+  ]);
 }
